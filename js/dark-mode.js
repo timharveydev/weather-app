66c6bc0b65967b9eae8a27ba88170d3f52015317
Tim Harvey <t.harvey1993@gmail.com>
2020-01-15 21:11:50 +0000

    Solved dark mode icon error

--- js/dark-mode.js
+++ js/dark-mode.js
@@ -8,22 +8,28 @@ let darkModeActive = false;
 // TOGGLE DARK MODE ON BUTTON CLICK
 modeButton.addEventListener('click', () => {
   if (darkModeActive == false) {
-    // Invert text and background colour
+    // Set icons to white
+    if (weather.iconId == undefined)
+      iconElement.innerHTML = `<img src="icons/white/unknown.png" alt="#">`;
+    else
+      iconElement.innerHTML = `<img src="icons/white/${weather.iconId}.png" alt="#">`;
+    // Invert colours & update btn text
     bodyElement.style.color = '#f9f9f9';
     containerElement.style.backgroundColor = '#131313';
-    // Update button text & set white icons
     modeButton.innerHTML = 'Light Mode';
-    iconElement.innerHTML = `<img src="icons/white/${weather.iconId}.png" alt="#">`;
 
     darkModeActive = true;
   }
   else {
-    // Invert text and background colour
+    // Set icons to black
+    if (weather.iconId == undefined)
+      iconElement.innerHTML = `<img src="icons/black/unknown.png" alt="#">`;
+    else
+      iconElement.innerHTML = `<img src="icons/black/${weather.iconId}.png" alt="#">`;
+    // Invert colours & update btn text
     bodyElement.style.color = '#0F0F0F';
     containerElement.style.backgroundColor = '#f9f9f9';
-    // Update button text & set black icons
     modeButton.innerHTML = 'Dark Mode';
-    iconElement.innerHTML = `<img src="icons/black/${weather.iconId}.png" alt="#">`;
 
     darkModeActive = false;
   }
